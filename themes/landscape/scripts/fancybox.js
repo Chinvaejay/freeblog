7278b7620fdfea1ebb6d104f23f578aa13d8013d
ChinVaejay <923983844@qq.com>
2022-01-12 01:05:00 +0800

    log

--- themes/landscape/scripts/fancybox.js
+++ themes/landscape/scripts/fancybox.js
@@ -1,5 +1,14 @@
 var rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/;
 
+console.log(`--
+---
+---
+---
+fancybox
+---
+---
+--`)
+
 /**
  * Fancybox tag
  *
@@ -16,6 +25,8 @@ hexo.extend.tag.register('fancybox', function(args) {
   }
 
   var title = args.join(' ');
+
+  console.log(title)
 
   if (title.startsWith('/source')) {
     thumbnail = 'https://cdn.jsdelivr.net/gh/Chinvaejay/freeblog'+title
